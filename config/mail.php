991080b13b082d3682202ef8ba96538470ec006a
arifulislamTmu <ariful0027@gmail.com>
2024-05-26 18:01:59 +0600

    Demo mailer last check

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => 'smtp.gmail.com' /* 'sandbox.smtp.mailtrap.io', */ /*  env('MAIL_HOST', 'smtp.mailgun.org') */,
-            'port' => /* 2525, */ env('MAIL_PORT', 587),
-            'encryption' => /* 'tls', */ env('MAIL_ENCRYPTION', 'tls'),
-            'username' => 'ariful0027@gmail.com'/* env('MAIL_USERNAME_email') */,
-            'password' => 'dwesshfndoudling', /*  env('MAIL_PASSWORD_pass') */
+            'host' => 'smtp.gmail.com',
+            'port' => 587,
+            'encryption' => 'tls',
+            'username' => 'ariful0027@gmail.com',
+            'password' => 'dwesshfndoudling',
             'timeout' => null,
         ],
 
