7725c77584b3f47ff264a38fe77ed083c269e900
arifulislamTmu <ariful0027@gmail.com>
2024-05-26 14:49:48 +0600

    Demo mailer

--- app/Http/Controllers/Frontend/AllproductController.php
+++ app/Http/Controllers/Frontend/AllproductController.php
@@ -166,16 +166,16 @@ $mail = new PHPMailer(true);
 try {
     $mail->SMTPDebug = 2;                                       
     $mail->isSMTP();                                            
-    $mail->Host       =env('MAIL_HOST');                    
+    $mail->Host       = 'smtp.gmail.com';                    
     $mail->SMTPAuth   = true;                             
-    $mail->Username   = env('MAIL_USERNAME');                 
-    $mail->Password   = env('MAIL_PASSWORD');                        
-    $mail->SMTPSecure = env('MAIL_ENCRYPTION');                              
-    $mail->Port       = env('MAIL_PORT'); 
+    $mail->Username   = 'ariful0027@gmail.com';                 
+    $mail->Password   = 'qpjpvukmemcdlqmd';                        
+    $mail->SMTPSecure = 'tls';                              
+    $mail->Port       = 587;  
  
     $mail->setFrom('from@gfg.com', 'Name');           
     $mail->addAddress('ariful0027@gmail.com');
-    $mail->addAddress('ariful0027@gmail.com', 'MPW-Wholsale');
+    $mail->addAddress('ariful0027@gmail.com', 'Name');
       
     $mail->isHTML(true);                                  
     $mail->Subject = 'Subject';
