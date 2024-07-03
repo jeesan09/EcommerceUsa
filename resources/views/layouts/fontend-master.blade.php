32cd1f28e8e63204184c618cfd7f624c630843d7
arifulislamTmu <ariful0027@gmail.com>
2024-07-03 22:44:40 +0600

    meta

--- resources/views/layouts/fontend-master.blade.php
+++ resources/views/layouts/fontend-master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google-site-verification" content="Dm5v2045IhsCrRknnn9GvOVnLan_y0hdqgOkgo9YOBM" />
   {{--   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
