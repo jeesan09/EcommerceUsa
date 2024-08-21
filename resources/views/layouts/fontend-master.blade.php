ffb632f7d29ef89f037ac1d8134e6ec54591c725
jeesan09 <jeesan09iub@gmail.com>
2024-08-21 19:25:14 +0600

    scarch console

--- resources/views/layouts/fontend-master.blade.php
+++ resources/views/layouts/fontend-master.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="google-site-verification" content="Dm5v2045IhsCrRknnn9GvOVnLan_y0hdqgOkgo9YOBM" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title> @yield('product_list')</title>
     @php
